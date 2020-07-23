6c9d1291cdb37a41b1506a6e32948f8111ab65f5
Charlie Barth <charliepbarth@gmail.com>
2020-07-23 21:19:51 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -18,7 +18,8 @@ class GetRequester
   
   def parse_json
     programs = JSON.parse(self.get_programs)
-  programs.collect do |program|
-    program["agency"]  
+    programs.collect do |program|
+      program["agency"]  
+    end
   end
 end
