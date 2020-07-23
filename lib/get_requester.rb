403643faa78c524351b256cbf802a114629f3695
Charlie Barth <charliepbarth@gmail.com>
2020-07-23 21:12:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,10 +2,11 @@ class GetRequester
   attr_accessor :url
   
   def initialize(url)
+    @url = url
   end 
   
   def get_response_body
-    uri = URI.parse(URL)
+    uri = URI.parse(@url)
   response = Net::HTTP.get_response(uri)
   response.body
   end
